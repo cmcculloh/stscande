c1bf902ddccb7c04125d8f2d9a50ac3cf9d00ff1
cmcculloh <cmcculloh@gmail.com>
2024-12-11 15:18:34 -0500

    Updates header nav a bit

--- src/lib/Header.svelte
+++ src/lib/Header.svelte
@@ -3,46 +3,63 @@
 </div>
 <nav class="navbar">
 	<div class="nav-left">
-		<div class="nav-item">
-			<a href="#">Home</a>
-			<div class="dropdown-content">
-				<a href="#">Submenu 1</a>
-				<a href="#">Submenu 2</a>
-			</div>
-		</div>
-		<div class="nav-item">
-			<a href="#">About</a>
-			<div class="dropdown-content">
-				<a href="#">Submenu 1</a>
-				<a href="#">Submenu 2</a>
-			</div>
-		</div>
-		<div class="nav-item">
-			<a href="#">Donate</a>
-			<div class="dropdown-content">
-				<a href="#">Submenu 1</a>
-				<a href="#">Submenu 2</a>
-			</div>
-		</div>
-		<div class="nav-item">
-			<a href="#">School</a>
-			<div class="dropdown-content">
-				<a href="#">Submenu 1</a>
-				<a href="#">Submenu 2</a>
-			</div>
-		</div>
-		<div class="nav-item">
-			<a href="#">Resources</a>
-			<div class="dropdown-content">
-				<a href="#">Submenu 1</a>
-				<a href="#">Submenu 2</a>
-			</div>
-		</div>
+		<ul class="nav-item">
+			<li><a href="/home">Home</a></li>
+		</ul>
+		<ul class="nav-item">
+			<li>
+				<a href="/about">About</a>
+				<ul class="dropdown-content">
+					<li><a href="/contact">Contact</a></li>
+					<li><a href="/leadership">Leadership</a></li>
+					<li><a href="/youth">Youth</a></li>
+					<li><a href="/music">Music</a></li>
+				</ul>
+			</li>
+		</ul>
+		<ul class="nav-item">
+			<li><a href="/donate">Donate</a></li>
+		</ul>
+		<ul class="nav-item">
+			<li><a href="/school">School</a></li>
+		</ul>
+		<ul class="nav-item">
+			<li>
+				<a href="/resources">Resources</a>
+				<ul class="dropdown-content">
+					<li><a href="/parish-directory">Parish Directory 🔒</a></li>
+					<li>
+						<a
+							href="https://www.google.com/calendar/embed?title=Calendar&amp;showTz=0&amp;height=600&amp;wkst=1&amp;bgcolor=%23ffffff&amp;src=dbb89q1e39rpomrlk7v8l5k2ug%40group.calendar.google.com&amp;color=%232952A3&amp;ctz=America%2FNew_York"
+							>Monthly Calendar ➡︎</a
+						>
+					</li>
+					<li>
+						<a href="https://www.youtube.com/channel/UCHl_paxhbDwNqv2MOZxU2jg">Streaming ➡︎</a>
+					</li>
+					<li><a href="https://oca.org/readings">Readings ➡︎</a></li>
+					<li><a href="https://roea.org/calendartipicpaschalia">Tipic ➡︎</a></li>
+					<li><a href="https://roea.org/">Episcopate ➡︎</a></li>
+				</ul>
+			</li>
+		</ul>
 	</div>
 	<div class="nav-right">
-		<a class="icon social-icon social-icon-youtube" href="https://www.youtube.com/channel/UCHl_paxhbDwNqv2MOZxU2jg" target="_blank">
-						<span class="screen-only">Youtube</span>
-						<svg class="e-font-icon-svg e-fab-youtube" viewBox="0 0 576 512" xmlns="http://www.w3.org/2000/svg"><path d="M549.655 124.083c-6.281-23.65-24.787-42.276-48.284-48.597C458.781 64 288 64 288 64S117.22 64 74.629 75.486c-23.497 6.322-42.003 24.947-48.284 48.597-11.412 42.867-11.412 132.305-11.412 132.305s0 89.438 11.412 132.305c6.281 23.65 24.787 41.5 48.284 47.821C117.22 448 288 448 288 448s170.78 0 213.371-11.486c23.497-6.321 42.003-24.171 48.284-47.821 11.412-42.867 11.412-132.305 11.412-132.305s0-89.438-11.412-132.305zm-317.51 213.508V175.185l142.739 81.205-142.739 81.201z"></path></svg>					</a>
+		<a
+			class="icon social-icon social-icon-youtube"
+			href="https://www.youtube.com/channel/UCHl_paxhbDwNqv2MOZxU2jg"
+			target="_blank"
+		>
+			<span class="screen-only">Youtube</span>
+			<svg
+				class="e-font-icon-svg e-fab-youtube"
+				viewBox="0 0 576 512"
+				xmlns="http://www.w3.org/2000/svg"
+				><path
+					d="M549.655 124.083c-6.281-23.65-24.787-42.276-48.284-48.597C458.781 64 288 64 288 64S117.22 64 74.629 75.486c-23.497 6.322-42.003 24.947-48.284 48.597-11.412 42.867-11.412 132.305-11.412 132.305s0 89.438 11.412 132.305c6.281 23.65 24.787 41.5 48.284 47.821C117.22 448 288 448 288 448s170.78 0 213.371-11.486c23.497-6.321 42.003-24.171 48.284-47.821 11.412-42.867 11.412-132.305 11.412-132.305s0-89.438-11.412-132.305zm-317.51 213.508V175.185l142.739 81.205-142.739 81.201z"
+				></path></svg
+			>
+		</a>
 		<input type="text" class="search-box" placeholder="Search..." />
 	</div>
 </nav>
@@ -66,12 +83,11 @@
 		display: flex;
 		justify-content: space-between;
 		align-items: center;
-		border-bottom-left-radius: 20px;
-		border-bottom-right-radius: 20px;
+		border-bottom-left-radius: var(--border-radius-s);
+		border-bottom-right-radius: var(--border-radius-s);
 		box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1);
 		padding: 0 20px;
 		font-family: var(--body-font);
-
 	}
 	a {
 		text-decoration: none;
@@ -94,7 +110,15 @@
 		background-color: white;
 		box-shadow: 0 8px 16px rgba(0, 0, 0, 0.1);
 		z-index: 1;
+		margin-top: 40px;
+		padding: 10px;
+		border-radius: var(--border-radius-s);
 	}
+
+	.dropdown-content li {
+		margin-top: 10px;
+	}
+
 	.nav-item:hover .dropdown-content {
 		display: block;
 	}
